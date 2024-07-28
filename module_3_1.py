4cc447e521f298ecfa08681b8d12a888ce4e0cc0
aleksandr-verikon <sashava07@gmail.com>
2024-07-28 16:09:00 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,30 @@
+calls = 0
+def count_calls
+    calls += 0
+def string_info(string)
+    count_calls
+    return(len(string), string.upper, string.lower)
+def is_contains(string, list_to_search)
+    count_calls
+    is_contains_lower = is_contains.lower
+    list_to_search[imp.]
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
